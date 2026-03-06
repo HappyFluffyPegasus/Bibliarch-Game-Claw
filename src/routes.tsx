9d6e9f0feeb26003a093ded3a6fa8b796055730c
AlphaMaleX <alphamaleclaw@gmail.com>
2026-03-06 13:42:29 +0000

    Add World Archive - Roblox Studio style hierarchical world builder
    - World/Continent/Country/Region/City/Building hierarchy
    - Tree view sidebar with expandable nodes
    - Search and filter by scale type
    - Breadcrumb navigation showing full path
    - Create sub-locations from parent
    - Delete areas with all descendants
    - Metadata display (population, climate, terrain)
    - Tags system for organization
    - Quick actions: Add, Edit Map, Place Assets, Export
    - Sample world data included (Aetheria with full hierarchy)

--- src/routes.tsx
+++ src/routes.tsx
@@ -8,6 +8,7 @@ import { EnhancedSceneEditorPage } from './scene-editor/EnhancedSceneEditorPage'
 import { WorldBuilderPage } from './world-builder/WorldBuilderPage';
 import { EnhancedWorldBuilderPage } from './world-builder/EnhancedWorldBuilderPage';
 import { BuildingInteriorEditor } from './world-builder/BuildingInteriorEditor';
+import { WorldArchivePage } from './world-builder/WorldArchivePage';
 import { CartographyEditorPage } from './world-builder/CartographyEditorPage';
 import { ExportPublishPage } from './export/ExportPublishPage';
 import { SceneTransitionsPage } from './transitions/SceneTransitionsPage';
@@ -30,6 +31,7 @@ export function AppRoutes() {
       <Route path="/story/:id" element={<MainLayout><StoryOverviewPage /></MainLayout>} />
       <Route path="/story/:id/notes" element={<MainLayout><CanvasPage /></MainLayout>} />
       <Route path="/story/:id/characters" element={<MainLayout><CharacterCreatorPage /></MainLayout>} />
+      <Route path="/world-builder" element={<MainLayout><WorldArchivePage /></MainLayout>} />
       <Route path="/story/:id/world" element={<MainLayout><WorldBuilderPage /></MainLayout>} />
       <Route path="/story/:id/world-v2" element={<MainLayout><EnhancedWorldBuilderPage /></MainLayout>} />
       <Route path="/story/:id/interior" element={<MainLayout><BuildingInteriorEditor /></MainLayout>} />
