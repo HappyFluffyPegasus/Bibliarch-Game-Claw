df54b4bf78007a2b078e20e4c84b4f47f260c50f
AlphaMaleX <alphamaleclaw@gmail.com>
2026-03-10 01:06:39 +0000

    Integrate Loading Screen and Command Palette into App
    - App now shows loading screen on startup
    - Progress bar animates to 100%
    - Command palette available globally (Cmd+K)
    - Smooth transition from loading to app
    
    Time: 01:06

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
 import { BrowserRouter } from 'react-router-dom'
 import { AppRoutes } from './routes'
+import { LoadingScreen } from './components/LoadingScreen'
+import { CommandPalette } from './components/CommandPalette'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [progress, setProgress] = useState(0)
+  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress(p => {
+        if (p >= 100) {
+          clearInterval(interval)
+          setTimeout(() => setIsLoading(false), 500)
+          return 100
+        }
+        return p + 20
+      })
+    }, 200)
+    
+    return () => clearInterval(interval)
+  }, [])
+  
+  if (isLoading) {
+    return <LoadingScreen progress={progress} />
+  }
+  
   return (
     <BrowserRouter>
       <AppRoutes />
+      <CommandPalette />
     </BrowserRouter>
   )
 }
